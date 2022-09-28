929f9cea502e8391475f0eca11082956d65265a7
xielong.wang <xielong.wang@nvr.cn>
2022-09-28 16:27:32 +0800

    - 测试发版

--- spring-cloud-kubernetes-coordinator/src/main/java/spring/cloud/kubernetes/coordinator/CoordinatorService.java
+++ spring-cloud-kubernetes-coordinator/src/main/java/spring/cloud/kubernetes/coordinator/CoordinatorService.java
@@ -2,8 +2,6 @@ package spring.cloud.kubernetes.coordinator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import spring.cloud.kubernetes.coordinator.config.Health;
 
 /**
  * @author wxl
@@ -11,11 +9,6 @@ import spring.cloud.kubernetes.coordinator.config.Health;
  */
 @SpringBootApplication(scanBasePackages = {"spring.cloud.kubernetes.coordinator", "org.apache.apisix.plugin.runner"})
 public class CoordinatorService {
-
-    @Bean(destroyMethod = "stop", initMethod = "start")
-    public Health health() {
-        return new Health();
-    }
 
     public static void main(String[] args) {
 
