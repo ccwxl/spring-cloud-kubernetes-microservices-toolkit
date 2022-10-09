28ae9b633367c59ef83032cd1bf51c85c81978d3
xielong.wang <xielong.wang@nvr.cn>
2022-10-09 11:31:03 +0800

    - 添加前端

---
+++ spring-cloud-kubernetes-configmap-ui/src/main/java/spring/cloud/kubernetes/configmap/ui/rest/ConfigMapController.java
@@ -0,0 +1,12 @@
+package spring.cloud.kubernetes.configmap.ui.rest;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author wxl
+ */
+@RestController
+public class ConfigMapController {
+
+
+}
