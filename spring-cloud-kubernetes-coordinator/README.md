ba4072b81f6e3cd8716f5513022a3374ae6eef05
siaron <siaron.wang@gmail.com>
2022-12-06 21:24:04 +0800

    - spring-cloud-kubernetes-coordinator支持native

---
+++ spring-cloud-kubernetes-coordinator/README.md
@@ -0,0 +1,30 @@
+## on linux maybe change
+
+- macos
+
+```json
+ {
+  "name": "io.netty.channel.kqueue.KQueueServerDomainSocketChannel",
+  "methods": [
+    {
+      "name": "<init>",
+      "parameterTypes": []
+    }
+  ]
+}
+
+```
+
+- linux
+
+```json
+ {
+  "name": "io.netty.channel.epoll.EpollServerDomainSocketChannel",
+  "methods": [
+    {
+      "name": "<init>",
+      "parameterTypes": []
+    }
+  ]
+},
+```
--- spring-cloud-kubernetes-coordinator/src/main/java/spring/cloud/kubernetes/coordinator/CoordinatorService.java
+++ spring-cloud-kubernetes-coordinator/src/main/java/spring/cloud/kubernetes/coordinator/CoordinatorService.java
@@ -1,5 +1,10 @@
 package spring.cloud.kubernetes.coordinator;
 
+import io.fabric8.kubernetes.api.model.Endpoints;
+import io.fabric8.kubernetes.api.model.EndpointsList;
+import io.fabric8.kubernetes.api.model.IntOrString;
+import io.fabric8.kubernetes.api.model.Service;
+import org.springframework.aot.hint.annotation.RegisterReflectionForBinding;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +12,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author wxl
  * 适配
  */
+@RegisterReflectionForBinding(value = {Service.class, IntOrString.Deserializer.class, Endpoints.class, EndpointsList.class})
 @SpringBootApplication(scanBasePackages = {"spring.cloud.kubernetes.coordinator", "org.apache.apisix.plugin.runner"})
 public class CoordinatorService {
 
